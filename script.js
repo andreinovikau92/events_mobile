ea5590fcc2a8bddd35c34e1ee712c1c69fdfc645
Andrei Novikau <andrei.novikau92@gmail.com>
2021-06-28 09:00:43 +0300

    touchend

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 window.addEventListener('DOMContentLoaded', () => {
     const box = document.querySelector('.box');
 
-    box.addEventListener('touchstart', (e) => {
+     box.addEventListener('touchstart', (e) => {
         e.preventDefault();
 
         console.log('touchstart');
+    }); 
+
+    box.addEventListener('touchmove', (e) => {
+        e.preventDefault();
+
+        console.log('move');
     });
 
     box.addEventListener('touchmove', (e) => {
         e.preventDefault();
 
-        console.log('touchmove');
+        console.log('move');
     });
 });
